b7de786518fd3c649408967b53d45a10676a1395
Vuong Tran <121814098+Vtrandev@users.noreply.github.com>
2024-05-20 22:43:20 -0700

    ToDo creation along with list addition function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 import Icon from "./image.jpg";
 import Header from "./Components/header";
 
-const toDoList = {}
+let toDoList = {}
 
 const btn = document.querySelector(".btn__add");
 const btnNew = document.querySelector(".btn__new");
@@ -10,28 +10,32 @@ const btnNew = document.querySelector(".btn__new");
 btn.addEventListener("click", (e) => component(e))
 btnNew.addEventListener("click", () => createToDo())
 
-function createToDo(e) {
-  const list = new ToDo("New", "This is a description", "Tomorrow", "Low", "Notes are here")
-
+function createToDo() {
+  // Create and initialize a list
+  const list = new ToDo("New Title", "This is a description", "Tomorrow", "Low", "Notes are here", {})
   console.log(list)
+  toDoList = list
 }
 
 function component(e) {
   e.preventDefault();
-  let id = Math.floor(Math.random()*10)
+  let id = 0
+  
+  while (toDoList.list[id]){
+    id = Math.floor(Math.random()*10)
+  }
+  
   const value = e.target.form[0].value
-  toDoList.id = {id, value}
+  toDoList.list[id] = {id, value}
 
-  console.log(toDoList);
+  for (let object in toDoList.list) {
+    console.log(toDoList.list[object])
+  }
+  // console.log(toDoList);
 }
 
-function ToDo(title, description, dueDate, priority, notes) {
-  // this.title = title;
-  // this.description = description;
-  // this.dueDate = dueDate;
-  // this.priority = priority;
-  // this.notes = notes;
-  return { title, description, dueDate, priority, notes }
+function ToDo(title, description, dueDate, priority, notes, list) {
+  return { title, description, dueDate, priority, notes, list }
 }
 
 
